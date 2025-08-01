368d2812735b4928d019fd199f7109da31de1e79
Yu (Jack) Wang <yu.wang.1@Vanderbilt.Edu>
2025-07-31 22:24:02 -0700

    Update run_traffic_explainer.sh

--- run_traffic_explainer.sh
+++ run_traffic_explainer.sh
@@ -1,44 +1,28 @@
-# echo ====================Byte-Transformer - ISCX VPN====================
+# echo ====================ISCX VPN====================
 python3 main.py --dataset='iscx-vpn' --baseline='Byte-Transformer'
 python3 test.py --dataset='iscx-vpn' --baseline='Byte-Transformer'
 python3 explain.py --dataset='iscx-vpn' --baseline='Byte-Transformer' --explanation='byte-level'
 python3 explain.py --dataset='iscx-vpn' --baseline='Byte-Transformer' --explanation='saliency map'
 python3 explain.py --dataset='iscx-vpn' --baseline='Byte-Transformer' --explanation='random'
 
-# echo ====================Byte-Transformer - ISCX Non-VPN====================
-# python3 main.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer'
-# python3 test.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer'
-# python3 explain.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer' --explanation='byte-level'
-# python3 explain.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer' --explanation='saliency map'
-# python3 explain.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer' --explanation='random'
+echo ====================ISCX Non-VPN====================
+python3 main.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer'
+python3 test.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer'
+python3 explain.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer' --explanation='byte-level'
+python3 explain.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer' --explanation='saliency map'
+python3 explain.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer' --explanation='random'
 
-# echo ====================Byte-Transformer - ISCX TOR====================
-# python3 main.py --dataset='iscx-tor' --baseline='Byte-Transformer'
-# python3 test.py --dataset='iscx-tor' --baseline='Byte-Transformer'
-# python3 explain.py --dataset='iscx-tor' --baseline='Byte-Transformer' --explanation='byte-level'
-# python3 explain.py --dataset='iscx-tor' --baseline='Byte-Transformer' --explanation='saliency map'
-# python3 explain.py --dataset='iscx-tor' --baseline='Byte-Transformer' --explanation='random'
+echo ====================ISCX TOR====================
+python3 main.py --dataset='iscx-tor' --baseline='Byte-Transformer'
+python3 test.py --dataset='iscx-tor' --baseline='Byte-Transformer'
+python3 explain.py --dataset='iscx-tor' --baseline='Byte-Transformer' --explanation='byte-level'
+python3 explain.py --dataset='iscx-tor' --baseline='Byte-Transformer' --explanation='saliency map'
+python3 explain.py --dataset='iscx-tor' --baseline='Byte-Transformer' --explanation='random'
 
-# echo ====================Byte-Transformer - ISCX Non-TOR====================
-# python3 main.py --dataset='iscx-nontor' --baseline='Byte-Transformer'
-# python3 test.py --dataset='iscx-nontor' --baseline='Byte-Transformer'
-# python3 explain.py --dataset='iscx-nontor' --baseline='Byte-Transformer' --explanation='byte-level'
-# python3 explain.py --dataset='iscx-nontor' --baseline='Byte-Transformer' --explanation='saliency map'
-# python3 explain.py --dataset='iscx-nontor' --baseline='Byte-Transformer' --explanation='random'
+echo ====================ISCX Non-TOR====================
+python3 main.py --dataset='iscx-nontor' --baseline='Byte-Transformer'
+python3 test.py --dataset='iscx-nontor' --baseline='Byte-Transformer'
+python3 explain.py --dataset='iscx-nontor' --baseline='Byte-Transformer' --explanation='byte-level'
+python3 explain.py --dataset='iscx-nontor' --baseline='Byte-Transformer' --explanation='saliency map'
+python3 explain.py --dataset='iscx-nontor' --baseline='Byte-Transformer' --explanation='random'
 
-
-# echo ====================ET-Bert - ISCX VPN====================
-# python3 main.py --dataset='iscx-vpn' --baseline='ET-Bert'
-# python3 test.py --dataset='iscx-vpn' --baseline='ET-Bert'
-
-# echo ====================ET-Bert - ISCX nonVPN====================
-# python3 main.py --dataset='iscx-nonvpn' --baseline='ET-Bert'
-# python3 test.py --dataset='iscx-nonvpn' --baseline='ET-Bert'
-
-# echo ====================ET-Bert - ISCX TOR====================
-# python3 main.py --dataset='iscx-tor' --baseline='ET-Bert'
-# python3 test.py --dataset='iscx-tor' --baseline='ET-Bert'
-
-# echo ====================ET-Bert - ISCX Non-TOR====================
-# python3 main.py --dataset='iscx-nontor' --baseline='ET-Bert'
-# python3 test.py --dataset='iscx-nontor' --baseline='ET-Bert'
