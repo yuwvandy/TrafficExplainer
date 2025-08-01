e1ec9164d000eaa7e663a10103ccd396eefc6114
Yu (Jack) Wang <yu.wang.1@Vanderbilt.Edu>
2025-07-31 22:24:16 -0700

    Update run_traffic_explainer.sh

--- run_traffic_explainer.sh
+++ run_traffic_explainer.sh
@@ -5,21 +5,21 @@ python3 explain.py --dataset='iscx-vpn' --baseline='Byte-Transformer' --explanat
 python3 explain.py --dataset='iscx-vpn' --baseline='Byte-Transformer' --explanation='saliency map'
 python3 explain.py --dataset='iscx-vpn' --baseline='Byte-Transformer' --explanation='random'
 
-echo ====================ISCX Non-VPN====================
+# echo ====================ISCX Non-VPN====================
 python3 main.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer'
 python3 test.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer'
 python3 explain.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer' --explanation='byte-level'
 python3 explain.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer' --explanation='saliency map'
 python3 explain.py --dataset='iscx-nonvpn' --baseline='Byte-Transformer' --explanation='random'
 
-echo ====================ISCX TOR====================
+# echo ====================ISCX TOR====================
 python3 main.py --dataset='iscx-tor' --baseline='Byte-Transformer'
 python3 test.py --dataset='iscx-tor' --baseline='Byte-Transformer'
 python3 explain.py --dataset='iscx-tor' --baseline='Byte-Transformer' --explanation='byte-level'
 python3 explain.py --dataset='iscx-tor' --baseline='Byte-Transformer' --explanation='saliency map'
 python3 explain.py --dataset='iscx-tor' --baseline='Byte-Transformer' --explanation='random'
 
-echo ====================ISCX Non-TOR====================
+# echo ====================ISCX Non-TOR====================
 python3 main.py --dataset='iscx-nontor' --baseline='Byte-Transformer'
 python3 test.py --dataset='iscx-nontor' --baseline='Byte-Transformer'
 python3 explain.py --dataset='iscx-nontor' --baseline='Byte-Transformer' --explanation='byte-level'
